98c1a9780a2291086b08e7866af897759189997d
yaaaan0 <chun8415@gmail.com>
2020-12-25 16:33:53 +0800

    setting

--- src/views/LeadingPage.vue
+++ src/views/LeadingPage.vue
@@ -1,12 +1,12 @@
-<template>
-    <v-container fluid >
-      <v-img  src="../assets/GPlogo.jpg" max-height="100"
-  max-width="100"></v-img>
-    <!-- <v-parallax class="fill-height" src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-parallax> -->
-    <div class="jarallax" data-jarallax-video="https://www.youtube.com/watch?v=BtsSnn481Yo" data-speed="0.1">
-      <p class="h-100 display-3 d-flex justify-content-center align-items-center text-white"></p>
-    </div>
-    </v-container>
+<template lang="pug">
+#leadingPage
+  v-container(fluid)
+    v-img(src='../assets/GPlogo.jpg' max-height='100' max-width='100')
+  //- .jarallax(data-jarallax-video='https://www.youtube.com/watch?v=TYJBXuEU-zQ' data-speed='0.1')
+  //-     p.h-100.display-3.d-flex.justify-content-center.align-items-center.text-white jarallax
+  v-container.w-100.h-100.d-flex.justify-center.align-end
+    v-row(align='center' justify='center')
+      v-btn( x-large rounded text fixed to="home") HOME
 </template>
 
 <script>
