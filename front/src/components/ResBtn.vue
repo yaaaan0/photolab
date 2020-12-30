35b83e7f451a108df7d52302aa28cd9c2ad4746c
yaaaan0 <chun8415@gmail.com>
2020-12-31 01:43:48 +0800

    setting login status

--- front/src/components/ResBtn.vue
+++ front/src/components/ResBtn.vue
@@ -1,11 +1,98 @@
 <template lang="pug">
 #resBtn
-  v-btn.btn-1(fab outlined medium to="login")
+  v-btn.btn-1(v-if="user.id.length === 0" fab outlined medium to="login")
     h6  登入註冊
+  v-btn.btn-1(v-if="user.id.length > 0" fab outlined medium to="user")
+    h6  會員專區
   v-btn.btn-2(fab outlined medium to="reserve")
     h6 預約
   </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    user () {
+      return this.$store.state.user
+    }
+  },
+  methods: {
+    logout () {
+      this.axios.delete(process.env.VUE_APP_API + '/users/logout')
+        .then(res => {
+          // 如果登出成功
+          if (res.data.success) {
+            this.$swal({
+              icon: 'success',
+              title: '成功',
+              text: '登出成功'
+            })
+
+            // 清除 vuex
+            this.$store.commit('logout')
+
+            // 導回首頁
+            if (this.$route.path !== '/') {
+              this.$router.push('/')
+            }
+          } else {
+            this.$swal({
+              icon: 'error',
+              title: '錯誤',
+              text: res.data.message
+            })
+          }
+        })
+        .catch(error => {
+          // 如果回來的狀態碼不是 200，直接 alert 錯誤訊息
+          this.$swal({
+            icon: 'error',
+            title: '錯誤',
+            text: error.response.data.message
+          })
+        })
+    },
+    heartbeat () {
+      this.axios.get(process.env.VUE_APP_API + '/users/heartbeat')
+        .then(res => {
+          // 如果 vuex 是登入中
+          if (this.user.id.length > 0) {
+            // 但是後端沒登入
+            if (!res.data) {
+              this.$swal({
+                icon: 'error',
+                title: '錯誤',
+                text: '登入時效已過'
+              })
+              // 登出
+              this.$store.commit('logout')
+              // 導回首頁
+              if (this.$route.path !== '/') {
+                this.$router.push('/')
+              }
+            }
+          }
+        })
+        .catch(() => {
+          this.$swal({
+            icon: 'error',
+            title: '錯誤',
+            text: '發生錯誤'
+          })
+          // 登出
+          this.$store.commit('logout')
+          // 導回首頁
+          if (this.$route.path !== '/') {
+            this.$router.push('/')
+          }
+        })
+    },
+    // 當vue元件準備好時 要做的動作
+    mounted () {
+      this.heartbeat()
+      setInterval(() => {
+        this.heartbeat()
+      }, 5000)
+    }
+  }
+}
 </script>
