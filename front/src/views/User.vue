8d961f3f3d5b7e72b6811e2212b313b98f964a17
yaaaan0 <chun8415@gmail.com>
2021-01-03 01:03:00 +0800

    setting logout

--- front/src/views/User.vue
+++ front/src/views/User.vue
@@ -1,9 +1,52 @@
 <template lang="pug">
   #user
+    UserMenu
+    v-btn(@click='logout') 登出
 </template>
 
 <script>
+import UserMenu from '../components/UserMenu.vue'
 export default {
-  name: 'user'
+  name: 'user',
+  components: {
+    UserMenu
+  },
+  methods: {
+    logout () {
+      this.axios.delete(process.env.VUE_APP_API + '/users/logout')
+        .then(res => {
+          // 如果登出成功
+          if (res.data.success) {
+            this.$swal({
+              icon: 'success',
+              iconColor: '#00000',
+              title: '登出成功'
+            })
+
+            // 清除 vuex
+            this.$store.commit('logout')
+
+            // 導回首頁
+            if (this.$route.path !== '/') {
+              this.$router.push('/')
+            }
+          } else {
+            this.$swal({
+              icon: 'error',
+              title: '錯誤',
+              text: res.data.message
+            })
+          }
+        })
+        .catch(error => {
+          // 如果回來的狀態碼不是 200，直接 alert 錯誤訊息
+          this.$swal({
+            icon: 'error',
+            title: '錯誤',
+            text: error.response.data.message
+          })
+        })
+    }
+  }
 }
 </script>
