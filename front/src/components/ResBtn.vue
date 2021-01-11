323c6593f924498025012168dc97e19a51e03e06
yaaaan0 <chun8415@gmail.com>
2021-01-11 16:37:03 +0800

    setting webmaster

--- front/src/components/ResBtn.vue
+++ front/src/components/ResBtn.vue
@@ -2,9 +2,11 @@
 #resBtn
   v-btn.btn-1(v-if="user.account.length === 0" fab outlined medium to="/login")
     h6  登入註冊
-  v-btn.btn-1(v-if="user.account.length > 0" fab outlined medium to="/user")
+  v-btn.btn-1(v-if="user.account.length > 0 && !user.account.includes('##')" fab outlined medium to="/user")
     h6  會員專區
-  v-btn.btn-2(fab outlined medium to="/reserve")
+  v-btn.btn-2(v-if="user.account.length > 0 && user.account.includes('##')" fab outlined medium to="/webmaster")
+    h6 網頁管理
+  v-btn.btn-2(v-if="(user.account.length > 0 && !user.account.includes('##')) || user.account.length === 0" fab outlined medium to="/reserve")
     h6 預約
   </template>
 
