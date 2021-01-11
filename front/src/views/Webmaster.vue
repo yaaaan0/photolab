fc028f9b7b91c6896b9309ac45185ed6fe561bc2
yaaaan0 <chun8415@gmail.com>
2021-01-11 16:38:05 +0800

    add pages

---
+++ front/src/views/Webmaster.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  #webmaster
+    UserMenu
+    vue-page-transition(name='fade-in-up')
+      router-view
+</template>
+
+<script>
+import UserMenu from '../components/UserMenu.vue'
+
+export default {
+  name: 'Webmaster',
+  components: {
+    UserMenu
+  },
+  methods: {
+  }
+}
+</script>
