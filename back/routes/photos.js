e57e532041f9ba568be85237dfeff561931abead
yaaaan0 <chun8415@gmail.com>
2021-01-15 16:33:14 +0800

    add edit

--- back/routes/photos.js
+++ back/routes/photos.js
@@ -1,10 +1,11 @@
 import express from 'express'
 
-import { create, del, allFile, file } from '../controllers/photos.js'
+import { create, del, allFile, file, edit } from '../controllers/photos.js'
 
 const router = express.Router()
 
 router.post('/', create)
+router.patch('/:id', edit)
 router.delete('/:id', del)
 router.get('/', allFile)
 router.get('/file/:file', file)
