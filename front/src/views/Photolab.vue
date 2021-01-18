0089cb3ed0a5c48be12e8b1ccc14af42c7d1e364
yaaaan0 <chun8415@gmail.com>
2021-01-18 16:35:54 +0800

    add likebtn && debug layout

--- front/src/views/Photolab.vue
+++ front/src/views/Photolab.vue
@@ -1,103 +1,48 @@
 <template lang="pug">
 #photolab
-  Photoswipe(v-if="aaa" bubble)
-    vueWaterfallEasy(ref="waterfall" :imgsArr="imgsArr" @scrollLoadImg="fetchImgsData" maxCols="6" @click="clickFn" @scrollReachBottom="getData" height="100vh")
-      .img-info(slot-scope="props")
-      v-img(:src="props.src" v-pswp="props")
-      div(slot='waterfall-over') end
-  v-layout(v-else justify-center)
-    div.grid( v-masonry="containerId" transition-duration='0.5s' stagger="0.03s" resize="true"  fit-width='true')
-      div(v-images-loaded:on.progress='imageProgress')
-        .item(v-masonry-tile v-for='(props, index) in imgsArr')
-          v-card(width="300")
-            Photoswipe(bubble)
-              v-img(:src="props.src" v-pswp="props" @load="$redrawVueMasonry('containerId')")
+  v-item-group(v-model="selected" multiple)
+    v-layout(justify-center)
+      div.grid
+        Photoswipe(bubble)
+          .item(v-masonry-tile v-for='(props, index) in imgsArr' :key="index")
+              v-card(width='300')
+                    v-img(:src="props.src" v-pswp="props")
+              v-item(v-slot="{ active, toggle }" :value="index")
+                v-btn(v-if="user.account.length > 0" icon :value="props" @click="click(props)")
+                  v-icon(color='rgba(255, 255, 255, 0.7)') {{ active ? 'mdi-heart' : 'mdi-heart-outline' }} {{active}}
 </div>
 
 </template>
 
 <script>
-import vueWaterfallEasy from 'vue-waterfall-easy'
-import imagesLoaded from 'vue-images-loaded'
 import imagesloaded from 'imagesloaded'
 import Masonry from 'masonry-layout'
 
 export default {
   name: 'Photolab',
-  directives: {
-    imagesLoaded
-  },
   data () {
     return {
-      aaa: false,
       imgsArr: [],
       props: '',
-      group: 0,
-      fetchImgsArr: [],
-      blocks: [],
-      isLoaded: false,
-      containerId: ''
+      containerId: '',
+      selected: []
     }
   },
-  components: {
-    vueWaterfallEasy
+  computed: {
+    user () {
+      return this.$store.state.user
+    }
   },
   methods: {
-    getData () {
-      this.axios.get(process.env.VUE_APP_API + '/photos/?group=' + this.group)
-        .then(res => {
-          this.group++
-          if (this.group === 5) {
-            this.$refs.waterfall.waterfallOver()
-            return
-          }
-          this.imgsArr = this.imgsArr.concat(res.data)
-        })
-    },
-    // initImgsArr () {
-    //   const arr = []
-    //   for (let i = 0; i < this.imgsArr.length; i++) {
-    //     arr.push({ src: this.imgsArr[i].src, link: '', info: '一些图片描述文字' })
-    //   }
-    //   console.log(arr)
-    //   return arr
-    // },
-
-    // fetchImgsData () { // 获取新的图片数据的方法，用于页面滚动满足条件时调用
-    //   this.imgsArr = this.imgsArr.concat(this.fetchImgsArr) // 数组拼接，把下一批要加载的图片放入所有图片的数组中
-    // },
-    // created () {
-    //   this.imgsArr = this.initImgsArr(0, 10) // 初始化第一次（即页面加载完毕时）要加载的图片数据
-    //   this.fetchImgsArr = this.initImgsArr(10, 20) // 模拟每次请求的下一批新的图片的数据数据
-    // },
-    created () {
-      this.getData()
-    },
-    // imgErrorFn (imgItem) {
-    //   console.log('圖片加載錯誤', imgItem)
-    // },
-    // clickFn (event, { index, value }) {
-    //   if (event.target.tagName.toLowerCase() === 'img') {
-    //     this.$swal({
-    //       title: `點擊圖片${index}`
-    //     })
-    //   }
-    // },
-    clickFn (event, { index, value }) {
-      this.$Pswp.open({
-        items: [
-          {
-            src: value.src,
-            title: value.title
-          }
-        ]
+    click (props) {
+      this.$swal({
+        icon: 'success',
+        iconColor: '#00000',
+        title: '收藏',
+        showConfirmButton: false,
+        timer: 1000
       })
-    },
-    imageProgress (instance, image) {
-      this.loaded = image.isLoaded ? 'loaded' : 'broken'
-    },
-    onImgLoad () {
-      this.isLoaded = true
+      console.log(props)
     }
   },
   updated () {
@@ -107,8 +52,9 @@ export default {
       fitWidth: 'true'
     })
     imagesloaded('.item', () => {
-      msnry.layout() // 图片加载完成后重新绘制。
-      console.log('789')
+      setTimeout(() => {
+        msnry.layout()
+      }, 100)
     })
   },
   mounted () {
@@ -123,9 +69,8 @@ export default {
             delete item.description
             return item
           })
+          this.imgsArr.reverse()
           console.log(this.imgsArr)
-          console.log(this.imgsArr.length)
-          this.$redrawVueMasonry()
         } else {
           this.$swal({
             icon: 'error',
