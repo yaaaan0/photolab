c924b054a8ae5c3bd9d192ed4ff001167423d01c
yaaaan0 <chun8415@gmail.com>
2021-01-22 01:42:46 +0800

    setting component

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -2,28 +2,16 @@
 #home
   #content
     v-app
-      HomeParallaxy
+      Banner
 </template>
 
 <script>
-import Menu from '../components/Menu.vue'
-import ResBtn from '../components/ResBtn.vue'
-import Word from '../components/Word.vue'
-import Footer from '../components/Footer.vue'
-import NevBtn from '../components/NevBtn.vue'
-import TopBtn from '../components/TopBtn.vue'
-import HomeParallaxy from '../components/HomeParallaxy.vue'
+import Banner from '../components/Banner.vue'
 
 export default {
   name: 'Home',
   components: {
-    Menu,
-    ResBtn,
-    Word,
-    Footer,
-    NevBtn,
-    TopBtn,
-    HomeParallaxy
+    Banner
   }
 }
 </script>
