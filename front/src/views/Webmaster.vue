8b33bcf8458c1f60e78f9f23fc33aa91ceb07210
yaaaan0 <chun8415@gmail.com>
2021-01-22 16:36:29 +0800

    add component

--- front/src/views/Webmaster.vue
+++ front/src/views/Webmaster.vue
@@ -1,19 +1,20 @@
 <template lang="pug">
   #webmaster
     UserMenu
+    TopBtn
     vue-page-transition(name='fade-in-up')
       router-view
 </template>
 
 <script>
 import UserMenu from '../components/UserMenu.vue'
+import TopBtn from '../components/TopBtn.vue'
 
 export default {
   name: 'Webmaster',
   components: {
-    UserMenu
-  },
-  methods: {
+    UserMenu,
+    TopBtn
   }
 }
 </script>
