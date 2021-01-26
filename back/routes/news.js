67ca2000590e693e8e51e61f5a6f990b3e0b777e
yaaaan0 <chun8415@gmail.com>
2021-01-27 02:37:21 +0800

    setting edit router

--- back/routes/news.js
+++ back/routes/news.js
@@ -1,13 +1,17 @@
 import express from 'express'
 
-import { New, allNews, create, del, edit } from '../controllers/news.js'
+import { New, allNews, create, del, imgEdit, textEdit } from '../controllers/news.js'
 
 const router = express.Router()
 
 router.get('/', allNews)
 router.get('/:id', New)
 router.post('/', create)
-router.patch('/:id', edit)
 router.delete('/:id', del)
+
+// 修改圖片和文字
+router.patch('/:id/imgEdit', imgEdit)
+// 修改純文字
+router.patch('/:id/textEdit', textEdit)
 
 export default router
