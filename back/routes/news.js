3c43ae05dd8abd67ffc03f2977bb0ecb9efc8e1b
yaaaan0 <chun8415@gmail.com>
2021-01-26 16:34:56 +0800

    add edit router

--- back/routes/news.js
+++ back/routes/news.js
@@ -1,13 +1,13 @@
 import express from 'express'
 
-import { New, allNews, create, del } from '../controllers/news.js'
+import { New, allNews, create, del, edit } from '../controllers/news.js'
 
 const router = express.Router()
 
 router.get('/', allNews)
 router.get('/:id', New)
 router.post('/', create)
-// router.patch('/:id', edit)
+router.patch('/:id', edit)
 router.delete('/:id', del)
 
 export default router
