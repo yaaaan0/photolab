0080f376fa84d7926c34f32e8bff77cde682b94f
yaaaan0 <chun8415@gmail.com>
2021-01-27 02:37:42 +0800

    setting

--- back/routes/users.js
+++ back/routes/users.js
@@ -1,8 +1,26 @@
 import express from 'express'
-import { create, login, logout, heartbeat, edit, addOrder, checkUser, allUser, editUser, checkOrder, addImage, checkImage, editImage, delImage } from '../controllers/users.js'
+import {
+  create,
+  login,
+  logout,
+  heartbeat,
+  edit,
+  addOrder,
+  checkUser,
+  allUser,
+  editUser,
+  checkOrder,
+  addImage,
+  checkImage,
+  editImage,
+  delImage
+  // addMsg,
+  // checkMsg
+} from '../controllers/users.js'
 
 const router = express.Router()
 
+// 使用者登入登出
 router.post('/signup', create)
 router.post('/login', login)
 router.delete('/logout', logout)
@@ -10,15 +28,24 @@ router.get('/heartbeat', heartbeat)
 
 // 查詢所有使用者
 router.get('/', allUser)
+// 修改訂單資訊
 router.patch('/:order_id', editUser)
 
+// 訂單
 router.patch('/:id', edit)
 router.get('/:id', checkUser)
 router.post('/order/:id', addOrder)
 router.get('/order/:id', checkOrder)
 
+// 收藏
 router.post('/image/:id', addImage)
 router.get('/image/:id', checkImage)
 router.patch('/image/:id', editImage)
 router.delete('/image/:id/:p_id', delImage)
+
+// 訊息
+// router.post('/message/:order_id', addMsg)
+// router.get('/message/:id/:order_id', checkMsg)
+// router.patch('/message/:id', editImage)
+// router.delete('/message/:id/:p_id', delImage)
 export default router
