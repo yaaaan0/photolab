268483ec964c16be964b040967feb9ae06400182
yaaaan0 <chun8415@gmail.com>
2021-01-28 16:36:00 +0800

    update router

--- front/src/components/ResBtn.vue
+++ front/src/components/ResBtn.vue
@@ -4,7 +4,7 @@
     h6  登入註冊
   v-btn.btn-1(v-if="user.account.length > 0 && !user.account.includes('##')" fab outlined medium to="/user")
     h6  會員專區
-  v-btn.btn-2(v-if="user.account.length > 0 && user.account.includes('##')" fab outlined medium to="/webmaster" target='_blank')
+  v-btn.btn-2(v-if="user.account.length > 0 && user.account.includes('##')" fab outlined medium to="/webmaster/allOrders" target='_blank')
     h6 網頁管理
   v-btn.btn-2(v-if="(user.account.length > 0 && !user.account.includes('##')) || user.account.length === 0" fab outlined medium to="/reserve")
     h6 預約
